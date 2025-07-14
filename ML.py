9e23ce97271a3d95bae18993e27517ee23755028
quinnieMA <qing.liang@zjxu.edu.cn>
2025-07-14 11:28:38 +0800

    litigation scoring work pipeline

---
+++ ML.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 14 11:26:03 2025
+
+@author: FM
+"""
+
+import cntext as ct
+import os
+from global_options import DataPaths
+
+# Initialize model with current directory and English setting
+model = ct.W2VModels(cwd=os.getcwd(), lang='english')
+
+# Train word2vec model using document corpus from global paths
+model.train(input_txt_file=str(DataPaths.PROCESSED_DIR / "processed_text.txt"))
+
+# Expand dictionary using seed words from global paths
+model.find(seedword_txt_file=str(DataPaths.SEEDWORDS_FILE), 
+           topn=100)
